9788ad0b511dfc8c5db055de2cf20e850ff25937
Cagri Türkseven <ct@canei.ag>
2023-01-30 17:26:50 +0100

    first commit

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,32 @@
+import "./login.scss";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>
+            Hello <br /> World.
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+            voluptatem nulla, earum sunt unde doloribus! Fugiat, exercitationem.
+            Facilis, porro nemo.
+          </p>
+          <span>Don't you have an account?</span>
+          <button>Register</button>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Passwort" />
+          </form>
+          <button>Login</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
