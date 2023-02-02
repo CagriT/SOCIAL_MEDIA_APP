8254392c2a75193cb19c23ee72330c4b71546bc2
Cagri Türkseven <ct@canei.ag>
2023-02-02 06:52:47 +0100

    icons added for top nav

--- src/components/LeftBar/LeftBar.jsx
+++ src/components/LeftBar/LeftBar.jsx
@@ -2,6 +2,12 @@ import "./leftBar.scss";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+
 import { Link } from "react-router-dom";
 
 const LeftBar = () => {
@@ -9,6 +15,13 @@ const LeftBar = () => {
     <div className="leftBar">
       <div className="left">
         <Link to="/">cagrisocial</Link>
+        <HomeOutlinedIcon />
+        <DarkModeOutlinedIcon />
+        <GridViewOutlinedIcon />
+        <div className="search">
+          <SearchOutlinedIcon />
+          <input />
+        </div>
       </div>
       <div className="right"></div>
     </div>
